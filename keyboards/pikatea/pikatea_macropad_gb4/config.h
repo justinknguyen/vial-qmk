a437c7a0e50ea10512c43eb6a2a57f947d22c38c
Christian Vespa <cjvespa@gmail.com>
2023-08-12 20:44:46 -0400

    Moved config items over to info.json

--- keyboards/pikatea/pikatea_macropad_gb4/config.h
+++ keyboards/pikatea/pikatea_macropad_gb4/config.h
@@ -17,67 +17,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "config_common.h"
-
-/* USB Device descriptor parameter */
-#define VENDOR_ID    0x5049
-#define PRODUCT_ID   0x001B
-#define DEVICE_VER   0x0001
-#define MANUFACTURER Jack_Kester
-#define PRODUCT      Pikatea Macropad GB4
-
-/* key matrix size */
-#define MATRIX_ROWS 2
-#define MATRIX_COLS 8
-
-/*
- * Keyboard Matrix Assignments
- *
- * Change this to how you wired your keyboard
- * COLS: AVR pins used for columns, left to right
- * ROWS: AVR pins used for rows, top to bottom
- * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
- *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
- *
- */
-#define MATRIX_COL_PINS { B6, B5, B4, B1, B0, E6, F0, D4 }
-#define MATRIX_ROW_PINS { F7, NO_PIN }
-
-/* COL2ROW, ROW2COL */
-#define DIODE_DIRECTION COL2ROW
-
-/* Encoder setup */
-#define ENCODERS 1
-
-#define ENCODERS_PAD_A { D6 }
-#define ENCODERS_PAD_B { D7 }
-
 #define ENCODERS_CW_KEY  { { 1, 1 } }
 #define ENCODERS_CCW_KEY { { 0, 1 } }
 
 /* RBG settings */
-#define RGB_DI_PIN C6
-
 #ifdef RGB_DI_PIN
-#    define RGBLED_NUM 7
-#    define RGBLIGHT_HUE_STEP 8
-#    define RGBLIGHT_SAT_STEP 8
-#    define RGBLIGHT_VAL_STEP 8
-#    define RGBLIGHT_LIMIT_VAL 150 /* The maximum brightness level */
-#    define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-#    define RGBLIGHT_LAYERS
 /*== all animations enable ==*/
 #    define RGBLIGHT_ANIMATIONS
-/*== or choose animations ==*/
-// #    define RGBLIGHT_EFFECT_BREATHING
-// #    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-// #    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-// #    define RGBLIGHT_EFFECT_SNAKE
-// #    define RGBLIGHT_EFFECT_KNIGHT
-// #    define RGBLIGHT_EFFECT_CHRISTMAS
-// #    define RGBLIGHT_EFFECT_STATIC_GRADIENT
-// #    define RGBLIGHT_EFFECT_RGB_TEST
-// #    define RGBLIGHT_EFFECT_ALTERNATING
 /*== customize breathing effect ==*/
 /*==== (DEFAULT) use fixed table instead of exp() and sin() ====*/
 #    define RGBLIGHT_BREATHE_TABLE_SIZE 256      // 256(default) or 128 or 64
@@ -89,7 +35,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
 
 /* define if matrix has ghost (lacks anti-ghosting diodes) */
 //#define MATRIX_HAS_GHOST
@@ -98,8 +43,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
-
-#define NO_USB_STARTUP_CHECK
 
 /* vial stuff */
 
