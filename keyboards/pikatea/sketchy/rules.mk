fe8480f1d9b758b925810e15125607ad8a71d1dc
Jack Kester <Jack.pikatea@gmail.com>
2023-09-25 18:00:17 -0500

    update encoders

--- keyboards/pikatea/sketchy/rules.mk
+++ keyboards/pikatea/sketchy/rules.mk
@@ -20,7 +20,6 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
-ENCODER_ENABLE = yes
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306
 
