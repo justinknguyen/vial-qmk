e4695f2eacc4f8d97e002a5f1d1fa6e85daf364d
Christian Vespa <cjvespa@gmail.com>
2024-02-05 10:15:15 -0500

    fix: updated test keymap

--- keyboards/pikatea/pikatea_macropad_r26/pikatea_macropad_r26_assembled/keymaps/test/keymap.c
+++ keyboards/pikatea/pikatea_macropad_r26/pikatea_macropad_r26_assembled/keymaps/test/keymap.c
@@ -28,26 +28,21 @@ enum layer_number {
 // };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE] = LAYOUT_all(
+    [_BASE] = LAYOUT(
         KC_1,  KC_2,   KC_A,    KC_3,    KC_4,
         KC_5,  KC_6,            KC_7,    KC_8
     ),
-    [_LOWER] = LAYOUT_all(
+    [_LOWER] = LAYOUT(
         KC_1,  KC_2,   KC_A,    KC_3,    KC_4,
         KC_5,  KC_6,            KC_7,    KC_8
     )
 };
 
 
-#ifdef ENCODER_ENABLE
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    // Volume control
-    if (!clockwise) {
-        tap_code(KC_VOLU);
-    } else {
-        tap_code(KC_VOLD);
-    }
-    return true;
-}
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+    [1] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+};
 #endif
 
