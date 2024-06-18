965cbe723a8b02df8cb83c0edfdc3f549501325f
Christian Vespa <cjvespa@gmail.com>
2024-06-18 09:01:54 -0400

    feat: removed a bunch of items from the config.h that now belong in info.json

--- keyboards/pikatea/pikatea_macropad_gb5/config.h
+++ keyboards/pikatea/pikatea_macropad_gb5/config.h
@@ -21,34 +21,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROWS 2
 #define MATRIX_COLS 6
 
-/*
- * Keyboard Matrix Assignments
- *
- * Change this to how you wired your keyboard
- * COLS: AVR pins used for columns, left to right
- * ROWS: AVR pins used for rows, top to bottom
- * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
- *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
- * GP17, GP18,
- */
-
-// #define MATRIX_COL_PINS { GP19, GP20, GP23, GP24, GP25, GP11  }
-// #define MATRIX_ROW_PINS { GP28, NO_PIN }
-
-
-/* COL2ROW, ROW2COL */
-// #define DIODE_DIRECTION COL2ROW
-
 /* Encoder setup */
 #define ENCODERS_CW_KEY  { { 1, 1 } }
 #define ENCODERS_CCW_KEY { { 0, 1 } }
 
 // /* RBG settings */
-// #define WS2812_DI_PIN GP7
-// #define WS2812_BYTE_ORDER WS2812_BYTE_ORDER_RGB
 #define NOP_FUDGE 0.4
-// #define NOP_FUDGE 1
-// #define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_BREATHING
 
 #ifdef WS2812_DI_PIN
 #    define RGBLIGHT_LAYERS
@@ -73,12 +51,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* default rgb */
 #    define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_BREATHING
 #endif
-
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
-
-/* define if matrix has ghost (lacks anti-ghosting diodes) */
-//#define MATRIX_HAS_GHOST
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
